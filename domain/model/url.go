d5e2be1ee2443a979f5896c8cbed148030efd2e9
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-12-16 09:48:34 +0900

    add url model

---
+++ domain/model/url.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/google/uuid"
+)
+
+// URL is URL info
+type URL struct {
+	ID   string `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+}
+
+// NewURL creates a new URL
+func NewURL(name string) (*URL, error) {
+	validURL := govalidator.IsURL(name)
+	if !validURL {
+		return nil, errors.New("name is not valid url.")
+	}
+
+	u, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	uu := u.String()
+
+	return &URL{
+		ID:   uu,
+		Name: name,
+	}, nil
+}
