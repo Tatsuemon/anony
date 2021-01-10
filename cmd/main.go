053f19609782b68da42cc776181ac36706519a28
Tatsuemon <i10mann-110@ezweb.ne.jp>
2021-01-10 23:35:36 +0900

    add db

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
     "fmt"
 	"net/http"
+	"log"
 	
 	"github.com/Tatsuemon/shortURL/infrastructure/datastore"
+	"github.com/Tatsuemon/shortURL/config"
 )
 
 func main() {
@@ -19,7 +21,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-	}
+	}()
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Sample")
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module shortURL
+module github.com/Tatsuemon/shortURL
 
 go 1.15
 
-require github.com/jmoiron/sqlx v1.2.0
+require (
+	github.com/go-sql-driver/mysql v1.4.0
+	github.com/jmoiron/sqlx v1.2.0
+)
