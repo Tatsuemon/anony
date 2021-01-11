cdd62f22407d39826a85aa736c71be554c0b9841
Tatsuemon <i10mann-110@ezweb.ne.jp>
2021-01-11 18:38:12 +0900

    add user repository

---
+++ domain/repository/user.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/Tatsuemon/anony/domain/model"
+)
+
+// UserRepository is a interface of UserRepository.
+type UserRepository interface {
+	FindAll() ([]*model.User, error)
+	FindByID(id string) ([]*model.User, error)
+	FindByName(name string) ([]*model.User, error)
+	FindByEmail(email string) ([]*model.User, error)
+	Store(ctx context.Context, user *model.User) (*model.User, error)
+	Update(ctx context.Context, user *model.User) (*model.User, error)
+	Delete(ctx context.Context, user *model.User) error
+}
