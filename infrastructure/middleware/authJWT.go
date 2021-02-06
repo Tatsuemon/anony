96bbfb2968699bb091affa0b3734d797d29c13ef
Tatsuemon <i10mann-110@ezweb.ne.jp>
2021-02-06 16:54:47 +0900

    add auth JWT

--- infrastructure/middleware/authJWT.go
+++ infrastructure/middleware/authJWT.go
@@ -2,27 +2,39 @@ package middleware
 
 import (
 	"context"
-	"fmt"
+	"errors"
+
+	"github.com/Tatsuemon/anony/domain/model"
+	"github.com/Tatsuemon/anony/domain/service"
 
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 )
 
 type AuthFunc func(ctx context.Context, fullMethodName string) (context.Context, error)
 
-func JWTAuth() AuthFunc {
+func JWTAuth(s service.UserService) AuthFunc {
 	return func(ctx context.Context, fullMethodName string) (context.Context, error) {
 		if isSkipMethod(fullMethodName) {
 			return ctx, nil
 		}
-		// TODO(Tatsuemon): ここで認可の処理を行う
-
 		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
-		fmt.Print(token)
 		if err != nil {
 			return nil, err
 		}
-		fmt.Print(token)
-
+		auth, err := model.ParseJWT(token)
+		if err != nil {
+			return nil, err
+		}
+		userID := auth.UserID
+		// ここで認可の処理を行う, userIDからUserIDを取得して, userが存在するかをみる
+		flag, err := s.ExistsID(userID)
+		if err != nil {
+			return nil, err
+		}
+		if !flag {
+			return nil, errors.New("token is invalid")
+		}
+		ctx = model.SetUserIDInContext(ctx, userID)
 		return ctx, nil
 	}
 }
